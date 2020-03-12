4a3040e38dd6b92f17fe283e2fcd986deb064966
sw <sw@sws-MacBook-Pro.local>
2020-03-12 07:12:12 -0600

    Added projects service and components: projects, projects-list, projects-details, toolbar

--- apps/sw18/src/app/app.module.ts
+++ apps/sw18/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
